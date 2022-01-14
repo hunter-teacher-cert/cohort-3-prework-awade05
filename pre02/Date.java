4c16df7f9e9b0a8000880bf45a037fe95a58b534
awade05 <51447771+awade05@users.noreply.github.com>
2022-01-14 03:26:43 +0000

    completed Time.java

---
+++ pre02/Date.java
@@ -0,0 +1,29 @@
+public class Date {
+public static void main(String[]args){
+
+String day = "Tuesday " ;
+int date = 11;
+String month = "January ";
+int year = 2022;
+System.out.print("Today's date is ");
+System.out.print(day);
+System.out.print(month);
+System.out.print(date + "th " );
+System.out.print( year);
+System.out.println(".");
+
+System.out.print(day+ ", ");
+System.out.print(month);
+System.out.print(date + ", " );
+System.out.print( year);
+System.out.println(".");
+
+System.out.print(day+ " ");
+System.out.print(month);
+System.out.print(date + " " );
+System.out.print( year);
+System.out.println(".");
+
+
+}
+}
---
+++ pre02/Time.java
@@ -0,0 +1,25 @@
+public class Time {
+public static void main(String[]args){
+  int hour = 14;
+  int minute = 53;
+  int sec = 30;
+  int timeOfDayInSeconds = 24 * 60 * 60;
+  int timesinceMidnightInSec = hour * 3600 + minute * 60 + sec;
+ int hournow = 21;
+ int minutenow = 59;
+ int secnow = 0;
+ int timeNow = hournow * 3600 + minutenow * 60 + secnow;
+  int elapsedTime = timeOfDayInSeconds - timeNow;
+
+System.out.print("Number of seconds since midnight: ");
+System.out.println(timesinceMidnightInSec);
+
+System.out.print("Number of seconds remaining in the day: ");
+System.out.println (timeOfDayInSeconds-timesinceMidnightInSec) ;
+
+System.out.print("Percentage of the day that has passed: ");
+System.out.println((double)timesinceMidnightInSec / (double)(24 * 60 *60) +"%");
+System.out.print("Elapsed time that has passed: ");
+System.out.println(elapsedTime);
+}
+}
