419e853f004e14e884d71bb805f10f973c3e4d2f
awade05 <51447771+awade05@users.noreply.github.com>
2022-04-22 06:25:59 +0000

    Craps in progress

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -1,8 +1,31 @@
 import java.util.*;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Craps {
+public static Random randomNumbers = new Random();
+  
+ //roll dice to get random number
+  public static int roll (int numRoll){
+   int randomNum = randomNumbers.nextInt(numRoll) + 1;
+   return randomNum;
+ }
+
+  //
+  public static int shoot (int numDice, int maxOfDice){
+    int shootDice = 0;
+    for(i = 0; i < numDice; i++){
+      shootDice += roll(maxOfDice/numDice);
+    }
+    return shootDice;
+  }
+
+  
+
+
+
+  
   public static void main(String[] args) {
-    public static int roll ()
+   
     } 
 }
